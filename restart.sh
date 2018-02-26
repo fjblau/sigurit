cb0e7022907421baaf4666a0f62a13d4857e9b47
fjblau <fjblau@gmail.com>
2018-02-26 09:34:18 +0100

    Fixed Restart shell

--- restart.sh
+++ restart.sh
@@ -3,3 +3,7 @@
 composer archive create -t dir -n . -a sigurit.bna
 composer network update -a sigurit.bna -c admin@sigurit
 composer-rest-server -c admin@sigurit -n never -w true
+
+# Fresh Install Only
+# composer runtime install -n sigurit -c PeerAdmin@hlfv1
+# composer network start --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw --archiveFile sigurit.bna --file networkadmin.card
